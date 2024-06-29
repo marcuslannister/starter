4b6850dd1e784e1eb4f10cc5c221e67a7e8d90af
marcuslannister <marcus@lannister.cc>
2024-06-29 18:21:04 +0800

    add new mappings for window manager

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,22 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Reload configuration without restart nvim
+map('n', '<leader>so', ':so %<CR>', { desc = "Runs Ex-commands" })
+-- Fast saving with <leader> and w
+-- map('n', '<leader>w', ':w<CR>')
+-- map('i', '<leader>w', '<C-c>:w<CR>')
+-- map('n', '<leader>bs', ':w<CR>')
+-- map('i', '<leader>bs', '<C-c>:w<CR>')
+
+-- Close all windows and exit from Neovim with <leader> and qq
+map('n', '<leader>qa', ':qa!<CR>')
+map('n', '<leader>qq', ':q<CR>')
+
+-- Manage window
+map('n', '<leader>wq', '<C-w><C-q>', { desc = "quit current window" })
+map('n', '<leader>wo', '<C-w><C-o>')
+map('n', '<leader>wv', '<C-w><C-v>')
+map('n', '<leader>ww', '<C-w><C-w>')
+

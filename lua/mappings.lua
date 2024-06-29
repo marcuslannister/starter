392307bca9e1a40c57810f6ad87154fecc7a4bfa
marcuslannister <marcus@lannister.cc>
2024-06-29 19:31:12 +0800

    add save buffer and edit mappings.lua mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,11 +11,12 @@ map("i", "jk", "<ESC>")
 
 -- Reload configuration without restart nvim
 map('n', '<leader>so', ':so %<CR>', { desc = "Runs Ex-commands" })
+
 -- Fast saving with <leader> and w
 -- map('n', '<leader>w', ':w<CR>')
 -- map('i', '<leader>w', '<C-c>:w<CR>')
--- map('n', '<leader>bs', ':w<CR>')
--- map('i', '<leader>bs', '<C-c>:w<CR>')
+map('n', '<leader>bs', ':w<CR>', { desc = "Save buffer" })
+map('i', '<leader>bs', '<C-c>:w<CR>', { desc = "Save buffer" })
 
 -- Close all windows and exit from Neovim with <leader> and qq
 map('n', '<leader>qa', ':qa!<CR>', { desc = "Quit all buffers without save" })
@@ -27,3 +28,5 @@ map('n', '<leader>wo', '<C-w><C-o>', { desc = "Close all other windows" })
 map('n', '<leader>wv', '<C-w><C-v>', { desc = "Split window vertically" })
 map('n', '<leader>ww', '<C-w><C-w>', { desc = "Switch windows" })
 
+-- Setting
+map('n', '<leader>em', ':e $HOME/.config/nvim/lua/mappings.lua<CR>')

55e04823fa2f6debee7e0f052da15799cb10f450
marcuslannister <marcus@lannister.cc>
2024-06-30 21:18:11 +0800

    add markdown snipmate snippet

--- lua/options.lua
+++ lua/options.lua
@@ -7,3 +7,6 @@ require "nvchad.options"
 
 -- vscode format i.e json files
 vim.g.vscode_snippets_path = vim.fn.stdpath "config" .. "/snippets/vscode"
+
+-- snipmate format 
+vim.g.vscode_snippets_path = vim.fn.stdpath "config" .. "/snippets/snipmate"
